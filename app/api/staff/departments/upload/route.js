18f5578e85899519385d99e248e404a8c7e2aa22
Emmanuel <emmanuelmakau90@gmail.com>
2026-05-28 16:46:26 +0300

    Fix import paths in staff departments upload API route - correct relative paths to libs

--- app/api/staff/departments/upload/route.js
+++ app/api/staff/departments/upload/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../libs/prisma";
-import cloudinary from "../../../../libs/cloudinary";
+import { prisma } from "../../../../../libs/prisma";
+import cloudinary from "../../../../../libs/cloudinary";
 
 export const dynamic = "force-dynamic";
 
